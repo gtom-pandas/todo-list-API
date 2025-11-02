4ec39fb1b5918786db46dca2c60db6fc7fde2d1e
Sotom <graci.tom21@gmail.com>
2025-11-02 10:21:55 +0100

    Update JWT secret and clean up auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'ton_secret_pour_jwt';
+const JWT_SECRET = 'jwt_secret';
 
 function authenticateToken(req, res, next) {
-  // Récupère le token du header Authorization
+  
   const authHeader = req.headers['authorization'];
   console.log("Authorization header:", req.headers['authorization']);
-  const token = authHeader && authHeader.split(' ')[1]; // Format attendu : "Bearer eyJhb..."
+  const token = authHeader && authHeader.split(' ')[1]; 
 
   if (!token) {
     return res.status(401).json({ message: 'Token manquant' });
@@ -13,7 +13,7 @@ function authenticateToken(req, res, next) {
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: 'Token invalide' });
-    req.user = user; // Ajoute l'id utilisateur dans req.user
+    req.user = user; 
     next();
   });
 }
